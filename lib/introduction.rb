48040595460a3180e271b486b0bfa474300231aa
Nicholas  Luong <nckluong@gmail.com>
2018-07-30 18:13:23 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 def introduction(name)
-  puts "Hi, my name is #{name} \n"
+  puts "Hi, my name is #{name}. \n"
 end 
 
 def introduction_with_language (name, lang)
-  puts "Hi, my name is #{name} and I am learning to program in #{lang} \n"
+  puts "Hi, my name is #{name} and I am learning to program in #{lang}. \n"
 end 
